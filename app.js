87a863fe0344fc10dd623051587f6822c1d39d0b
Sumit Saurabh <sumitsaurabh927@gmail.com>
2021-08-23 15:44:12 +0530

    added gifs

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ var current = document.querySelector("#current")
 var qty = document.querySelector("#qty")
 var btn = document.querySelector("#btn")
 var out = document.querySelector("#output")
+var profitGif = document.querySelector("#profit")
+var lossGif = document.querySelector("#loss")
+var nothingGif = document.querySelector("#nothing")
+
+
+profitGif.style.display = "none";
+lossGif.style.display = "none";
+nothingGif.style.display = "none";
 
 function onClick() {
     var iniPrice = Number(initial.value);
@@ -14,14 +22,27 @@ btn.addEventListener("click", onClick)
 
 function showResults(ini, fin, qty) {
     if (ini > fin) {
+        nothingGif.style.display = "none";
+
+        profitGif.style.display = "none";
+        nothingGif.style.display = "none";
         var loss = (ini - fin) * qty;
         var lossPc = ((loss) / ini) * 100
-        console.log("loss of " + loss + "\n i.e,. " + lossPc + "%");
+        out.innerText = "Loss of ₹" + loss + "\n i.e., " + lossPc + "%"
+        lossGif.style.display = "block";
+
     } else if (ini < fin) {
+        lossGif.style.display = "none";
+        nothingGif.style.display = "none";
         var profit = (fin - ini) * qty
         var profitPc = ((profit) / ini) * 100
-        console.log("profit of " + profit + "\n i.e,. " + profitPc + "%");
+        out.innerText = "Profit of ₹" + profit + "\n i.e., " + profitPc + "%"
+        profitGif.style.display = "block";
     } else {
-        console.log("better luck next time");
+        profitGif.style.display = "none";
+        lossGif.style.display = "none";
+        out.innerText = "Better luck next time"
+        nothingGif.style.display = "block";
+
     }
 }
